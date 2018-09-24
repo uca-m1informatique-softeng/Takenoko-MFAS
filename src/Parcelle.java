d4799a4761be7159e530e0c2de7d2ade0f20374b
Arthur Doglio <arthur.doglio@etu.unice.fr>
2018-09-24 13:54:04 +0200

    #2 Implementation des parcelles étangs

--- src/Parcelle.java
+++ src/Parcelle.java
@@ -1,6 +1,12 @@
 
 public class Parcelle {
-    public Parcelle(){}
+    final TypeParcelle type; //Pour le moment il y a que etang
+    private boolean irrigue;
+
+    public Parcelle(TypeParcelle type, boolean irrigue){
+        this.type = type;
+        this.irrigue = irrigue;
+    }
 
     @Override
     public String toString() {
