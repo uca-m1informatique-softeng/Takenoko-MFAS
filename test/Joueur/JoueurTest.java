005064092bd9201a11827a191fbe3b3f1940345d
sofiane djema <sofiane.djema@etu.unice.fr>
2018-10-07 18:09:35 +0200

    #41 Test unitaire

--- test/Joueur/JoueurTest.java
+++ test/Joueur/JoueurTest.java
@@ -1,24 +1,26 @@
 package Joueur;
 
-import Joueur.Bot;
-import Moteur.*;
+import Moteur.Enums;
 import Moteur.Objectifs.ObjectifJardinier;
+import Moteur.Parcelle;
+import Moteur.Partie;
+import Moteur.Plateau;
 import javafx.geometry.Point3D;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
-import Moteur.Enums.*;
 
-public class BotTest {
+public class JoueurTest {
+
     Partie partie=new Partie();
     Plateau pla =partie.getPlateau();
-    Parcelle par = new Parcelle(TypeParcelle.etang);
-    Bot j = new Bot(CouleurBot.Rouge);
+    Parcelle par = new Parcelle(Enums.TypeParcelle.Jaune);
+    Bot j = new Bot(Enums.CouleurBot.Rouge);
 
 
     @Test
-    public void verifierMonObjectif(){
-        ObjectifJardinier ob = new ObjectifJardinier(6,TypeParcelle.Jaune,4);
+    public void verifierMesObjectifs(){
+        ObjectifJardinier ob = new ObjectifJardinier(6, Enums.TypeParcelle.Jaune,4);
 
         /*On crée notre plateau composé que de parcelles jaunes*/
 
@@ -29,6 +31,7 @@ public class BotTest {
 
         /*On donne un objectif au jardinier */
         j.AddObjectif(ob);
+        System.out.println("score "+j.getScore());
 
         /*on fait pousser du bambou de taille 3 sur une parcelle (objectif pas réalisé)*/
         for(int i = 0 ; i <3 ; i++){
@@ -36,14 +39,14 @@ public class BotTest {
         }
 
         j.verifierMesObjectif(partie);
-
         assertEquals(0,j.getNombreObjectifs());
 
         ///// Test 2 /////
 
-        /*on fait pousser du bambou de taille 3 sur une parcelle (objectif pas réalisé)*/
+        /*on fait pousser du bambou de taille 4 sur une parcelle (objectif réalisé)*/
         pla.getParcelle(new Point3D(0,-1,1)).pousserBambou();
         j.verifierMesObjectif(partie);
+
         assertEquals(1,j.getNombreObjectifs());
     }
 
@@ -73,14 +76,14 @@ public class BotTest {
         assertEquals(new Point3D(0,1,-1),partie.getJardinier().getCoord());
     }
 
-    //Ca existe plus
-    /*
+
     @Test
-    public void play(){
-        partie.getDeck().piocheObjectifJardinier(j);
-        j.play(partie);
-        assertEquals(new Point3D(0,1,-1),partie.getPlateau().getKeylist().get(1));//on vérifie si le joueur a posé
-        assertEquals(new Point3D(0,1,-1),partie.getJardinier().getCoord());//on vérifie si le joueur a deplacé le jardinier
-        assertEquals(0,j.getNombreObjectifs());
-    }*/
+    public void joueurDeplacePanda(){
+        pla.poser(par,new Point3D(0,1,-1));
+        pla.poser(par,new Point3D(1,0,-1));
+
+        j.joueurDeplacePanda(partie.getPanda());
+
+        assertEquals(new Point3D(0,1,-1),partie.getPanda().getCoord());
+    }
 }
--- test/Moteur/DeckTest.java
+++ test/Moteur/DeckTest.java
@@ -7,18 +7,49 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class DeckTest {
-    /*
-    Deck d = new Deck();
+    Partie partie = new Partie();
+    Deck d = new Deck(partie);
+
+
+    @Test
+    public void initialiserDeckParcelle() throws Exception {
+        Parcelle[] dp = new Parcelle[28];
+        for (int i = 0;i < 11;i++){
+            dp[i] = new Parcelle(Enums.TypeParcelle.Verte);
+        }
+        for (int i = 11;i < 18;i++){
+            dp[i] = new Parcelle(Enums.TypeParcelle.Rose);
+        }
+        for (int i = 18;i < 27;i++){
+            dp[i] = new Parcelle(Enums.TypeParcelle.Jaune);
+        }
+        for (int i =0 ; i<27;i++){
+            assertEquals(d.getDeckParcelle()[i].getType().toString(),dp[i].getType().toString());
+        }
+
+    }
 
 
     @Test
     public void initialiserObjectifsJardinier() throws Exception {
-        ObjectifJardinier [] deckjar = new ObjectifJardinier[3];
-        deckjar[0] = new ObjectifJardinier(6,TypeParcelle.Jaune,4);
-        deckjar[1] = new ObjectifJardinier(7,TypeParcelle.Rose,4);
-        deckjar[2] = new ObjectifJardinier(5,TypeParcelle.Vert,4);
+        ObjectifJardinier [] deckjar = new ObjectifJardinier[12];
+        deckjar[0] = new ObjectifJardinier(6, Enums.TypeParcelle.Jaune,4);
+        deckjar[1] = new ObjectifJardinier(6, Enums.TypeParcelle.Jaune,4);
+        deckjar[2] = new ObjectifJardinier(6, Enums.TypeParcelle.Jaune,4);
+        deckjar[3] = new ObjectifJardinier(6, Enums.TypeParcelle.Jaune,4);
 
-        for (int i =0 ; i<3;i++){
+        deckjar[4] = new ObjectifJardinier(7, Enums.TypeParcelle.Rose,4);
+        deckjar[5] = new ObjectifJardinier(7, Enums.TypeParcelle.Rose,4);
+        deckjar[6] = new ObjectifJardinier(7, Enums.TypeParcelle.Rose,4);
+        deckjar[7] = new ObjectifJardinier(7, Enums.TypeParcelle.Rose,4);
+
+        deckjar[8] = new ObjectifJardinier(5,Enums.TypeParcelle.Verte,4);
+        deckjar[9] = new ObjectifJardinier(5,Enums.TypeParcelle.Verte,4);
+        deckjar[10] = new ObjectifJardinier(5,Enums.TypeParcelle.Verte,4);
+        deckjar[11] = new ObjectifJardinier(5,Enums.TypeParcelle.Verte,4);
+
+
+        for (int i =0 ; i<12;i++){
             assertEquals(d.getDeckObjectifsJardinier()[i].getValeur(),deckjar[i].getValeur());
             assertEquals(d.getDeckObjectifsJardinier()[i].getCouleur(),deckjar[i].getCouleur());
             assertEquals(d.getDeckObjectifsJardinier()[i].getTailleBambou(),deckjar[i].getTailleBambou());
@@ -27,16 +58,21 @@ public class DeckTest {
 
     @Test
     public void initialiserObjectifsPanda() throws Exception {
-        ObjectifPanda [] deckpan = new ObjectifPanda[3];
-        deckpan[0] = new ObjectifPanda(4,TypeParcelle.Jaune,2);
-        deckpan[1] = new ObjectifPanda(5,TypeParcelle.Rose,2);
-        deckpan[2] = new ObjectifPanda(3,TypeParcelle.Vert,2);
+        ObjectifPanda [] deckpan = new ObjectifPanda[9];
+        deckpan[0] = new ObjectifPanda(4,Enums.TypeParcelle.Jaune,2);
+        deckpan[1] = new ObjectifPanda(4,Enums.TypeParcelle.Jaune,2);
+        deckpan[2] = new ObjectifPanda(4,Enums.TypeParcelle.Jaune,2);
+        deckpan[3] = new ObjectifPanda(5,Enums.TypeParcelle.Rose,2);
+        deckpan[4] = new ObjectifPanda(5,Enums.TypeParcelle.Rose,2);
+        deckpan[5] = new ObjectifPanda(5,Enums.TypeParcelle.Rose,2);
+        deckpan[6] = new ObjectifPanda(3,Enums.TypeParcelle.Verte,2);
+        deckpan[7] = new ObjectifPanda(3,Enums.TypeParcelle.Verte,2);
+        deckpan[8] = new ObjectifPanda(3,Enums.TypeParcelle.Verte,2);
 
-        for (int i =0 ; i<3;i++){
+        for (int i =0 ; i<9;i++){
             assertEquals(d.getDeckObjectifsPanda()[i].getValeur(),deckpan[i].getValeur());
             assertEquals(d.getDeckObjectifsPanda()[i].getCouleur(),deckpan[i].getCouleur());
             assertEquals(d.getDeckObjectifsPanda()[i].getNombreBambou(),deckpan[i].getNombreBambou());
         }
-    }*/
-
+    }
 }
