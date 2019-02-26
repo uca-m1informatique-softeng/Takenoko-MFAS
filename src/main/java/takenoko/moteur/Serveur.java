5afe4016dda432286a586ea12fdac779cb26c991
Arthur Doglio <arthur.doglio@etu.unice.fr>
2019-02-26 19:19:13 +0100

    #11 Gestion des interactions Serveur

--- src/main/java/takenoko/moteur/Serveur.java
+++ src/main/java/takenoko/moteur/Serveur.java
@@ -20,4 +20,21 @@ public class Serveur {
         this.plateau.resetPlateau();
         return "Bienvenue";
     }
+    @Path("/Parcelle")
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String Par() {
+        stock.SetStock(deck.piocherParcelle(),plateau.emplacementsAutorise());
+        return "1 - "+stock.StockParcelle.get(0).getType().toString()+"\n"+"2 - "+stock.StockParcelle.get(1).getType().toString()+"\n"+"3 - "+stock.StockParcelle.get(2).getType().toString()+"\n \n"+
+                "1 - "+stock.StockPoint.get(0).toString()+"\n"+"2 - "+stock.StockPoint.get(1).toString()+"\n"+"3 - "+stock.StockPoint.get(2).toString() ;
+    }
+
+
+    @Path("/Poser/{parcelle}/{position}")
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String Poser(@PathParam("parcelle") int parcelle, @PathParam("position")int position){
+        plateau.poser(stock.StockParcelle.get(parcelle-1),stock.StockPoint.get(position-1));
+        return "Parcelle posee avec succes";
+    }
 }
